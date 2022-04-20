7837c5b8397367a5fd07204acd1cc40da39f45d2
Christopher Woodall <woodall.christopher@gmail.com>
2022-04-20 12:46:59 -0400

    move included code into its own directory

--- src/inc/disks.asm
+++ src/inc/disks.asm
@@ -17,11 +17,30 @@ loop_disks:
 ; =------------------------------------------------------=
 write_disk:
   push dx
+    ; mov ah, 0x08
+    ; mov ax, [bootdev]
+    ; stc       ; Adjust Carry -INT 0x13 will clean it if there is no error
+    ; pusha
+    ;   int 0x13
+    ; popa
+    ; jnc .done
+    ; DH = (NUM_HEADS - 1)
+    ; CL = SECTORS_PER_TRACK
+    and cl,0
+    mov cl, 1
+    jmp .loop
+  pop dx
+  cmp dh,al
+  jne .done
+  ret
+
+  .loop:
     mov ah, 0x03
     mov bx, DUST
     mov	al, 1	; Write one sector
     mov	ch, 0	; First track
-    mov	cl, 1	; First sector
+
+    ; mov	cl, 1	; First sector
     mov	dh, 0	; First head - assume only one
     mov dl, [bootdev]
     stc       ; Adjust Carry -INT 0x13 will clean it if there is no error
@@ -29,10 +48,10 @@ write_disk:
       int 0x13
     popa
     jnc .done
-  pop dx
-  cmp dh,al
-  jne .done
-  ret
+    inc cl
+    cmp cl, 17
+    je .done
+
 
   .done:
     pop dx
