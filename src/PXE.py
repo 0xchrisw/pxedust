2b0fb5551b744612ae1148bb9f6cfea5bb32842c
Christopher Woodall <woodall.christopher@gmail.com>
2022-04-20 12:47:44 -0400

    server prototype

---
+++ src/PXE.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys; sys.dont_write_bytecode=True;
+
+__author__      = "Christopher Woodall"
+
+
+from scapy.all import *
+from scapy.base_classes import Net
+conf.verbose = 0
+conf.iface   = 'guest-bridge'
+
+
+class PXE( AnsweringMachine ):
+  iface_ip = get_if_addr( conf.iface )
+  ports = ' || '.join(
+    str( port ) for port in
+    [ 67, 68, 69, 80, 135, 137, 138, 139, 389, 443, 4011, 5040 ]
+  )
+
+  filter = f'( arp or tcp or udp ) and port ( {ports} ) and !( src host {get_if_addr( conf.iface )} )'
+  sniff_options = {"store": 0, "iface": conf.iface }
+  #send_options = {"verbose": 0}  # type: Dict[str, Any]
+  #send_options_list = ["iface", "inter", "loop", "verbose", "socket"]
+  send_function = staticmethod( sendp )
+
+
+  def is_request(self, pkt):
+    return pkt.haslayer( IP )
+
+  def make_reply( self, pkt ):
+    pkt.summary( )
+    return IP( )
+
+  def dhcp( self, pkt ):
+    pass
+
+  def tftp( self, pkt ):
+    pass
+
+  def http( self, pkt ):
+    pass
+
+
+PXE( )( )
