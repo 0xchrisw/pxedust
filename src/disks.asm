178bfd2511966420364e9fe15adcebf474989fb4
Christopher Woodall <woodall.christopher@gmail.com>
2022-04-19 21:40:57 -0400

    add error checking

--- src/disks.asm
+++ src/disks.asm
@@ -16,18 +16,24 @@ loop_disks:
 
 ; =------------------------------------------------------=
 write_disk:
-  mov ah, 0x03
-  mov bx, DUST
-  mov	al, 1	; Write one sector
-  mov	ch, 0	; First track
-  mov	cl, 1	; First sector
-  mov	dh, 0	; First head - assume only one
-  mov dl, [bootdev]
-  ; stc    ; Adjust Carry -INT 0x13 will clean it if there is no error
-  ; pusha
-  int 0x13
-  ; popa
+  push dx
+    mov ah, 0x03
+    mov bx, DUST
+    mov	al, 1	; Write one sector
+    mov	ch, 0	; First track
+    mov	cl, 1	; First sector
+    mov	dh, 0	; First head - assume only one
+    mov dl, [bootdev]
+    stc       ; Adjust Carry -INT 0x13 will clean it if there is no error
+    pusha
+      int 0x13
+    popa
+    jnc .done
+  pop dx
+  cmp dh,al
+  jne .done
   ret
 
-
-
+  .done:
+    pop dx
+    ret
