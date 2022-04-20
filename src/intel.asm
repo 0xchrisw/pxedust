78ed1287da3bb899c608e85ff7061300dc277eca
Christopher Woodall <woodall.christopher@gmail.com>
2022-04-19 21:42:23 -0400

    Update intel.asm

--- src/intel.asm
+++ src/intel.asm
@@ -23,3 +23,32 @@ clear_screen:
 	int 0x10
 	ret
 
+
+print_hex:
+	mov bx,dx
+	and bx,0x000f
+	cmp bl,0x09
+	jg print_letter
+	add bx,0x30
+	mov ah,0x0e
+	mov al,bl
+	int 0x10
+	shr dx,4
+	rol dx,8
+	and dx,dx
+	jnz print_hex
+	ret
+
+
+print_letter:
+	sub bl,0x09
+	dec bl
+	add bl,0x61
+	mov ah,0x0e
+	mov al,bl
+	int 0x10
+	shr dx,4
+	rol dx,8
+	and dx,dx
+	jnz print_hex
+	ret
