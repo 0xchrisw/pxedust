4df258020b1a4594ad85c5ada3e43accac9b3ac4
Christopher Woodall <woodall.christopher@gmail.com>
2022-04-19 21:29:30 -0400

    use fast a20 gate and update screen blanking

--- src/intel.asm
+++ src/intel.asm
@@ -2,26 +2,24 @@
 
 ; Activate A20
 A20:
-	mov ax, 0x2401
-	int 0x15
+	; Fast A20 Gate.
+  mov al, 2
+  out 0x92, al
+	ret
 
 
 ; Move cursor to top left position
 move_to_orig:
-	mov ah, 0x02
-	xor bx, bx
-	xor dx, dx
-	int 0x10
+	mov ah, 0x02	; Set cursor position function
+	xor bx, bx		; Page
+	xor dx, dx		; Column
+	int 0x10			; BIOS interrupt
+	ret
 
 
 ; Clear screen
 clear_screen:
-	mov ah, 0x06
-	xor al, al
-	xor bx, bx
-	mov bh, 0x07
-	xor cx, cx
-	mov dh, 24
-	mov dl, 79
+	mov ax, 0x02
 	int 0x10
+	ret
 
